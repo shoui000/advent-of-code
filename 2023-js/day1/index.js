b450cdd645c04811e671fe5b2b06f89495f0a092
Shoui-Laptop <dimydeveza@gmail.com>
2023-12-29 12:30:53 -0300

    Refactoring: renaming the files and grouping them

---
+++ 2023-js/day1/index.js
@@ -0,0 +1,51 @@
+import { readFile } from 'node:fs';
+
+const numbers = {
+    oneight: '18',
+    twone: '21',
+    threeight: '38',
+    fiveight: '58',
+    sevenine: '78',
+    eightwo: '82',
+    eighthree: '83',
+    nineight: '98',
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
+};
+
+readFile('./data.txt', (err, info) => {
+    if (err) throw err;
+
+    let input = info.toString('utf8').split('\n');
+
+    function puzzle(input, secondPuzzle){
+        let sum = 0;
+        input.forEach((item) => {
+            if (secondPuzzle) {
+                for (const [key, value] of Object.entries(numbers)){
+                    item = item.replaceAll(key, value);
+                };
+            };
+
+            const regex = /[0-9]/g;
+            const found  = item.match(regex);
+            if (found){
+                const number = Number(`${found[0]}${found[found.length-1]}`);
+                sum += number;
+            }
+        })
+        return sum;
+    };
+    const firstPuzzle = puzzle(input, false);
+    const secondPuzzle = puzzle(input, true);
+
+    console.log(`First Answer: ${firstPuzzle}`);
+    console.log(`Second Answer: ${secondPuzzle}`);
+});
--- 2023-js/day2/index.js
+++ 2023-js/day2/index.js
@@ -3,12 +3,12 @@ import { readFile } from 'node:fs';
 readFile('./data.txt', (err, input) => {
     if (err) throw err;
 
-    let info = input.toString("utf8").split('\n');
+    let info = input.toString("utf8").split('\n').slice(0, -1);
     // info = info.slice(0, 4); // Just some to test;
 
     function parseGame(gameString){
         let index = gameString.split(':')[0].slice('5');
-        let content = gameString.slice(8);
+        let content = gameString.split(':')[1];
 
         let sets = content.split(';');
         let parsedSets = sets.map(set => set = set.split(',').map(x => x.trim()));
@@ -59,17 +59,30 @@ readFile('./data.txt', (err, input) => {
         return {game: game.game, possible: isPossible};
     };
 
+    function extractPowers (game){
+        let power = game.colors.red * game.colors.blue * game.colors.green;
+        return {game: game.game, power: power, colors: {...game.colors}};
+    };
+
     function main() {
         let parsedInfo = info.map(parseGame);
         let trueGamers = parsedInfo.map(extractTrueGamers).filter(x => x.possible);
+        let extractedPowers = parsedInfo.map(extractPowers).filter(x => x.game != '');
 
-        let sum = 0;
+        let sumFirst = 0;
         trueGamers.forEach(game => {
             if (game.game){
-                sum += Number(game.game)
+                sumFirst += Number(game.game)
             }
         });
-        console.log(sum)
+
+        let sumSecond = 0;
+        extractedPowers.forEach(game => {
+            sumSecond += game.power;
+        });
+
+        console.log(`First Answer: ${sumFirst}`);
+        console.log(`Second Answer: ${sumSecond}`);
     };
 
     main()
