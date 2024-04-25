7928ed4ce76e599a1a4639e1f98d377939884e08
shoui <dimydeveza@gmail.com>
2024-04-25 18:54:19 -0300

    first puzzle complete (day1)

--- 2023/day1/main.go
+++ 2023/day1/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+    "bufio"
+    "regexp"
+    "strconv"
+)
 
 func main(){
-    fmt.Println("Hello, vagabunda")
+    file, err := os.Open("data.txt")
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    var sum = 0
+    for scanner.Scan() {
+        re := regexp.MustCompile(`[0-9]`)
+        lineNumber := re.FindAllString(scanner.Text(), -1)
+
+        numberString := lineNumber[0] + lineNumber[len(lineNumber) -1]
+        
+        if s, err := strconv.Atoi(numberString); err == nil{
+            sum += s
+        }
+    }
+
+    fmt.Println(sum)
+
+    if err := scanner.Err(); err != nil {
+        fmt.Println(err)
+    }
 }
