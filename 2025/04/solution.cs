1f286e29aefebed164c37c618d1641b4e399a1dc
Dimitry Deveza <dimydeveza@gmail.com>
2025-12-04 10:01:03 -0300

    second puzzle of day 04 of 2025

--- 2025/04/solution.cs
+++ 2025/04/solution.cs
@@ -13,8 +13,10 @@ class Program {
 
 		Solution s = new Solution(option);
 		int fpSolution = s.FirstPuzzle();
+		int spSolution = s.SecondPuzzle();
 
 		Console.WriteLine($"FirstPuzzle: The answer is {fpSolution}");
+		Console.WriteLine($"SecondPuzzle: The answer is {spSolution}");
 
 	}
 
@@ -61,6 +63,54 @@ class Solution {
 				if (localSum < 4) sum++;
 			}
 		}
+
+		return sum;
+	}
+
+	public int SecondPuzzle() {
+		char[,] matrix = new char[input.Length, input[0].Length];
+
+		for (int i = 0; i < input.Length; i++) {
+			for (int j = 0; j < input[0].Length; j++) {
+				matrix[i, j] = input[i][j];
+			}
+		}
+
+		int sum = 0;
+		int localSum = 0;
+		int removed = 0;
+		int rows = input.Length;
+		int cols = input[0].Length;
+
+		do {
+			removed = 0;
+
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < cols; j++) {
+					if (matrix[i, j] == '.' || matrix[i, j] == 'X') continue;
+
+					localSum = 0;
+
+					if (i > 0 && j > 0 && matrix[i-1, j-1] == '@') localSum++;
+					if (i > 0 && matrix[i-1, j] == '@') localSum++;
+					if (i > 0 && j < cols - 1 && matrix[i-1, j+1] == '@') localSum++;
+
+					if (j > 0 && matrix[i, j-1] == '@') localSum++;
+					if (j < cols - 1 && matrix[i, j+1] == '@') localSum++;
+
+					if (i < rows - 1 && j > 0 && matrix[i+1, j-1] == '@') localSum++;
+					if (i < rows - 1 && matrix[i+1, j] == '@') localSum++;
+					if (i < rows - 1 && j < cols - 1 && matrix[i+1, j+1] == '@') localSum++;
+
+					if (localSum < 4) {
+						sum++; removed++;
+						matrix[i, j] = 'X';
+					}
+
+				}
+			}
+
+		} while (removed > 0);
 
 		return sum;
 	}
